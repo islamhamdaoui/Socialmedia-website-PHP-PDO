3012498e37e2c54b8aed02dd8c7886f5eb7f2d86
islamhamdaoui <islamhamdaoui2000@gmail.com>
2024-07-04 22:25:24 +0100

    database updated

--- login.sql
+++ login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 03 juil. 2024 à 21:47
+-- Généré le : jeu. 04 juil. 2024 à 23:24
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `content`, `created_at`) VALUES
+(2, 2, 'hi everyone ', '2024-07-04 20:28:27'),
+(3, 2, 'hi everyone ', '2024-07-04 20:29:40'),
+(4, 2, 'hh', '2024-07-04 20:29:52'),
+(5, 1, 'hiii', '2024-07-04 20:30:23'),
+(20, 1, 'new post', '2024-07-04 21:01:06'),
+(21, 2, 'so this is a new post man\\', '2024-07-04 21:16:24'),
+(22, 2, 'hi guys again', '2024-07-04 21:18:05');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `users`
 --
 
@@ -39,11 +65,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'islam', 'islama', 'islamputh@gmail.com');
+(1, 'islam', 'islama', 'islamputh@gmail.com'),
+(2, 'islamputh', 'islam123', 'islamhamdaoui@gmail.com');
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Index pour la table `users`
@@ -56,10 +90,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT pour la table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
