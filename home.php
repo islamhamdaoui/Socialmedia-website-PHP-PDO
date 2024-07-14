d5a41444dde57d370ceecaf15d2978cb6c233afd
islamhamdaoui <islamhamdaoui2000@gmail.com>
2024-07-14 13:35:07 +0100

    likes issue fixed -git some help from chatgpt damn it

--- home.php
+++ home.php
@@ -98,71 +98,83 @@ require("connection.php");
 
 $user = $_SESSION["user_id"];
 
-$show = $db->query('SELECT posts.id as post_id, posts.content, DATE(posts.created_at) as post_date, users.username,verified, users.id as user_id, likes.user_id as liker_id,likes.post_id as likespost_id, users.pdp, COUNT(DISTINCT comments.id) as comments_count, COUNT(DISTINCT likes.user_id) as likes_count
-                   FROM posts 
-                   INNER JOIN users ON posts.user_id = users.id 
-                   LEFT JOIN comments ON posts.id = comments.post_id
-                   LEFT JOIN likes on posts.id = likes.post_id
-                   GROUP BY 
-                        posts.id, posts.content, users.username, users.id, users.pdp
-                   ORDER BY posts.created_at DESC');
 
-// Display posts and user information
+
+$show = $db->prepare('SELECT 
+posts.id as post_id, 
+posts.content, 
+DATE(posts.created_at) as post_date, 
+users.username,
+users.verified, 
+users.pdp as pdp,
+users.id as user_id, 
+COUNT(DISTINCT comments.id) as comments_count, 
+COUNT(DISTINCT likes.id) as likes_count,
+SUM(CASE WHEN likes.user_id = ? THEN 1 ELSE 0 END) as liked_by_user
+FROM 
+posts 
+INNER JOIN 
+users ON posts.user_id = users.id 
+LEFT JOIN 
+comments ON posts.id = comments.post_id
+LEFT JOIN 
+likes ON posts.id = likes.post_id
+GROUP BY 
+posts.id, posts.content, users.username, users.id
+ORDER BY 
+posts.created_at DESC');
+
+$show->execute([$user]);
+
 
 while ($data = $show->fetch()) {
-    echo '<div class="post" id="post_' . $data['post_id'] . '">';
-    echo "<div class='username' onclick=\"";
-    if ($data['username'] === $_SESSION['username']) {
-        echo "window.location.href = 'profile.php';";
-    } else {
-        echo "window.location.href = 'info.php?id={$data['user_id']}';";
-    }
-    echo "\">";
-    
-    if ($data['pdp'] === 'default') {
-        echo '<img src="uploads/default.png" alt="default Image">';
-    } else {
-        echo "<img src='uploads/{$data['pdp']}.png' alt='{$data['pdp']} Image'>";
-    }
-
-    
-    echo "<div class='userdiv' >";
-    echo "<div class='usertop'>";
-    echo "<h3 >" . htmlspecialchars($data['username']) . '</h3>';
-    if($data['verified']) {
-    echo "<img src='icons/verified.png' class='verified'>";
+echo '<div class="post" id="post_' . $data['post_id'] . '">';
+echo "<div class='username' onclick=\"";
+if ($data['username'] === $_SESSION['username']) {
+echo "window.location.href = 'profile.php';";
+} else {
+echo "window.location.href = 'info.php?id={$data['user_id']}';";
 }
-    echo "</div>";
-    echo "<span>" .$data['post_date'] . "</span>";
-    echo '</div>';
+echo "\">";
 
-    echo "</div>";
-    echo '<p>' . htmlspecialchars($data['content']) . '</p>'; 
-   
-    
-    echo "<div class='reactions'>";
-    if ( $data['liker_id'] == $user && $data['user_id'] !== $user && $data['likespost_id'] ===$data['post_id'] ) { 
-        echo "<div onclick=\"window.location.href='dislike.php?id={$data['post_id']}'\">";
-        echo "<img class='like' src='icons/liked.png'> ";
-        echo '<span class="like-count">' . $data['likes_count'] . ' Likes</span>';
-        echo '</div>';
-    } elseif ($data['user_id'] !== $user) { // Display like button if the user is not the owner
-        echo "<div onclick=\"window.location.href='like.php?post_id={$data['post_id']}&owner_id={$data['user_id']}'\">";
-        echo "<img class='like' src='icons/like.png'> ";
-        echo '<span class="like-count">' . $data['likes_count'] . ' Likes</span>';
-        echo '</div>';
-    } else { // Display like count only to the post owner
-        echo '<div>';
-        echo '<span class="like-count">' . $data['likes_count'] . ' Likes</span>';
-        echo '</div>';
-    }
-    
-    echo "<div  onclick=\"window.location.href='postview.php?id={$data['post_id']}'\"><img src='icons/comment.png'> {$data['comments_count']} Comment</div> ";
-    echo "<div><img src='icons/view.png'> View post</div>";
-    echo "</div>";
- 
-    
-    echo '</div>';
+if ($data['pdp'] === 'default') {
+echo '<img src="uploads/default.png" alt="default Image">';
+} else {
+echo "<img src='uploads/{$data['pdp']}.png' alt='{$data['pdp']} Image'>";
+}
+
+echo "<div class='userdiv'>";
+echo "<div class='usertop'>";
+echo "<h3>" . htmlspecialchars($data['username']) . '</h3>';
+if ($data['verified']) {
+echo "<img src='icons/verified.png' class='verified'>";
+}
+echo "</div>";
+echo "<span>" . $data['post_date'] . "</span>";
+echo '</div>';
+echo "</div>";
+echo '<p>' . htmlspecialchars($data['content']) . '</p>'; 
+
+echo "<div class='reactions'>";
+if ($data['liked_by_user'] > 0) { 
+
+echo "<div onclick=\"window.location.href='dislike.php?id={$data['post_id']}'\">";
+echo "<img class='like' src='icons/liked.png'> ";
+echo '<span class="like-count">' . $data['likes_count'] . ' Likes</span>';
+echo '</div>';
+} else { 
+
+echo "<div onclick=\"window.location.href='like.php?post_id={$data['post_id']}&owner_id={$data['user_id']}'\">";
+echo "<img class='like' src='icons/like.png'> ";
+echo '<span class="like-count">' . $data['likes_count'] . ' Likes</span>';
+echo '</div>';
+} 
+
+echo "<div  onclick=\"window.location.href='postview.php?id={$data['post_id']}'\"><img src='icons/comment.png'> {$data['comments_count']} Comment</div> ";
+echo "<div><img src='icons/view.png'> View post</div>";
+echo "</div>";
+
+echo '</div>';
 }
 echo '</div>';
 } 
