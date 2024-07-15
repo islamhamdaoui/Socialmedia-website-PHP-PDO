e0239415f90a68cc32aa744a36a8df545088d0b8
islamhamdaoui <islamhamdaoui2000@gmail.com>
2024-07-15 14:47:37 +0100

    notifications improved

--- delete/deletecomment.php
+++ delete/deletecomment.php
@@ -1,11 +1,18 @@
 <?php
 
 require("../connection.php");
-
+session_start();
+$user_id =  $_SESSION['user_id'];
 $id = $_GET['id'];
 $post_id = $_GET['post_id'];
 $delete = $db ->prepare('DELETE FROM comments WHERE id = :id');
 $delete -> execute(array('id'=> $id));
+
+$dislike = $db -> prepare('DELETE FROM notifications WHERE user_id = :user_id && post_id = :post_id');
+$dislike -> execute(array(
+    'user_id'=> $user_id,
+    'post_id'=> $post_id
+ ));
 
 header("location:../postview.php?id=$post_id");
 exit();
